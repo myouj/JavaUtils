5d0ba8afbbd3f836513a06cafd442e9b7fdef5ee
myouj <18565544225@163.com>
2019-04-05 23:58:47 +0800

    读取文件到byte[]中，byte[]写到文件中

--- src/com/ma/common/utils/File2ByteArray.java
+++ src/com/ma/common/utils/File2ByteArray.java
@@ -2,7 +2,7 @@ package com.ma.common.utils;
 
 import java.io.*;
 
-public class FileUtils {
+public class File2ByteArray {
 
     /**
      * 把文件写入到byte[]数组中
@@ -39,32 +39,5 @@ public class FileUtils {
         fos.write(code);
         fos.close();
     }
-
-    /**
-     * 把文件的内容写到string字符串中
-     * @param filePath
-     * @return
-     * @throws IOException
-     */
-    static String readFile(String filePath) throws IOException {
-        StringBuffer sb = new StringBuffer();
-        readToBuffer(sb, filePath);
-        return sb.toString();
-    }
-
-    static void readToBuffer(StringBuffer buffer, String filePath) throws IOException {
-        InputStream is = new FileInputStream(filePath);
-        String line;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-        line = reader.readLine();
-        while (line != null){
-            buffer.append(line);
-            buffer.append("\n");
-            line = reader.readLine();
-        }
-        reader.close();
-        is.close();
-    }
-
 
 }
